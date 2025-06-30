5a8735f7906b278470a03b7131fc346d4e7fcbb9
riyatyag <tyagiriya517@gmail.com>
2025-06-30 17:02:49 +0530

    Day 25

--- LongestHarmoniousSubsequence.java
+++ LongestHarmoniousSubsequence.java
@@ -50,8 +50,7 @@ class Solution {
         for (int num : nums) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-
-        int maxLength = 0;
+         int maxLength = 0;
         for (int num : freqMap.keySet()) {
             if (freqMap.containsKey(num + 1)) {
                 maxLength = Math.max(maxLength, freqMap.get(num) + freqMap.get(num + 1));
