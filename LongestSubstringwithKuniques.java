78f491a34a7e1f3f7f4753e735d9f3cd5e1d42cb
riyatyag <tyagiriya517@gmail.com>
2025-07-03 11:17:29 +0530

    Day 27

--- LongestSubstringwithKuniques.java
+++ LongestSubstringwithKuniques.java
@@ -21,8 +21,9 @@ class Solution {
     public int longestKSubstr(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
         
-        int maxLength = -1; // Initialize result
-        int left = 0; // Left boundary of window
+        int maxLength = -1; 
+        int left = 0; 
+        // Left boundary of window
 
         for (int right = 0; right < s.length(); right++) {
             char ch = s.charAt(right);
