0f953cc78f2a213b253c9793c6ca06135b2bf63f
riyatyag <tyagiriya517@gmail.com>
2025-07-04 09:50:30 +0530

    Day 28

--- FindtheK-thcharacterstringgameII.java
+++ FindtheK-thcharacterstringgameII.java
@@ -34,13 +34,13 @@
 
 class Solution {
   public char kthCharacter(long k, int[] operations) {
-    final int operationsCount = (int) Math.ceil(Math.log(k) / Math.log(2));
-    int increases = 0;
+     final int operationsCount = (int) Math.ceil(Math.log(k) / Math.log(2));
+      int increases = 0;
 
     for (int i = operationsCount - 1; i >= 0; --i) {
       final long halfSize = 1L << i;
       if (k > halfSize) {
-        k -= halfSize; // Move k from the right half to the left half.
+        k -= halfSize;
         increases += operations[i];
       }
     }
