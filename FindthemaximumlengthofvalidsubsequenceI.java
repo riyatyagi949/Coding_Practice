f21249cc2bbf628e9ebc7a7345ab6d2bddc31635
riyatyag <tyagiriya517@gmail.com>
2025-07-16 12:08:59 +0530

    Day 40

--- FindthemaximumlengthofvalidsubsequenceI.java
+++ FindthemaximumlengthofvalidsubsequenceI.java
@@ -25,7 +25,6 @@ class Solution {
     public int maximumLength(int[] nums) {
         int maxLength = 0;
 
-        // 1. Even-Odd-Even... starting with even-index parity = 0
         // 2. Odd-Even-Odd... starting with even-index parity = 1
         for (int evenParity = 0; evenParity <= 1; evenParity++) {
             for (int oddParity = 0; oddParity <= 1; oddParity++) {
