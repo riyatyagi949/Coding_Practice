42bd2035ab9d4a2b6e5e8e9ea93a6caed294cfb6
riyatyag <tyagiriya517@gmail.com>
2025-07-26 10:03:49 +0530

    Day 50

--- MajorityelementII.java
+++ MajorityelementII.java
@@ -30,7 +30,8 @@ class Solution {
             } 
             else if (num == candidate2) {
                 count2++;
-            } else if (count1 == 0) {
+            } 
+            else if (count1 == 0) {
                 candidate1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
