0147f306764280419f8fd70b48b33c9b331f6664
riyatyag <tyagiriya517@gmail.com>
2025-07-27 10:45:42 +0530

    Day 51

--- Setmatrixzeroes.java
+++ Setmatrixzeroes.java
@@ -54,7 +54,6 @@ class Solution {
         boolean[] row = new boolean[n];
         boolean[] col = new boolean[m];
 
-        // First pass to mark rows and columns
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (mat[i][j] == 0) {
