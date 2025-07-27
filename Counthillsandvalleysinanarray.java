9a6ee14cefa1c7d2190e9cb5ce63087d25e911ac
riyatyag <tyagiriya517@gmail.com>
2025-07-27 10:51:36 +0530

    Day 51

--- Counthillsandvalleysinanarray.java
+++ Counthillsandvalleysinanarray.java
@@ -28,13 +28,12 @@ class Solution {
     public int countHillValley(int[] nums) {
         List<Integer> clean = new ArrayList<>();
         clean.add(nums[0]);
+        
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
                 clean.add(nums[i]);
             }
         }
-
-        // Step 2: Count hills and valleys
         int count = 0;
         for (int i = 1; i < clean.size() - 1; i++) {
             if (clean.get(i) > clean.get(i - 1) && clean.get(i) > clean.get(i + 1)) {
