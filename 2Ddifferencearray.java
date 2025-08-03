5068dc75211640c3f0f7cadd64c0ff98737a0d77
riyatyag <tyagiriya517@gmail.com>
2025-08-03 10:37:48 +0530

    Day 58

--- 2Ddifferencearray.java
+++ 2Ddifferencearray.java
@@ -40,47 +40,6 @@
  * * Space Complexity: O(n * m)
  * - We need an additional 2D difference array of size O(n*m).
  */
-class Solution {
-    public int[][] solve(int[][] mat, int[][] opr) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] diff = new int[n + 1][m + 1];
 
-        for (int[] op : opr) {
-            int v = op[0];
-            int r1 = op[1];
-            int c1 = op[2];
-            int r2 = op[3];
-            int c2 = op[4];
 
-            diff[r1][c1] += v;
-            if (c2 + 1 < m) {
-                diff[r1][c2 + 1] -= v;
-            }
-            if (r2 + 1 < n) {
-                diff[r2 + 1][c1] -= v;
-            }
-            if (r2 + 1 < n && c2 + 1 < m) {
-                diff[r2 + 1][c2 + 1] += v;
-            }
-        }
-
-        int[][] res = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i > 0) {
-                    diff[i][j] += diff[i - 1][j];
-                }
-                if (j > 0) {
-                    diff[i][j] += diff[i][j - 1];
-                }
-                if (i > 0 && j > 0) {
-                    diff[i][j] -= diff[i - 1][j - 1];
-                }
-                res[i][j] = mat[i][j] + diff[i][j];
-            }
-        }
-
-        return res;
-    }
-}
+       
