077cba39ff6f759f2cf5b771284eb24be9093169
riyatyag <tyagiriya517@gmail.com>
2025-08-06 11:11:51 +0530

    Day 61

--- Romannumbertointeger.java
+++ Romannumbertointeger.java
@@ -43,30 +43,3 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class Solution {
-    public int romanToInteger(String s) {
-        Map<Character, Integer> romanValues = new HashMap<>();
-        romanValues.put('I', 1);
-        romanValues.put('V', 5);
-        romanValues.put('X', 10);
-        romanValues.put('L', 50);
-        romanValues.put('C', 100);
-        romanValues.put('D', 500);
-        romanValues.put('M', 1000);
-
-        int result = 0;
-        int prevValue = 0;
-
-        for (int i = s.length() - 1; i >= 0; i--) {
-            int currentValue = romanValues.get(s.charAt(i));
-            if (currentValue < prevValue) {
-                result -= currentValue;
-            } else {
-                result += currentValue;
-            }
-            prevValue = currentValue;
-        }
-
-        return result;
-    }
-}
