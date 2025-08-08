4e9696c1e4ccf0500c79f95593676066f344ec8f
riyatyag <tyagiriya517@gmail.com>
2025-08-08 13:27:28 +0530

    Day 63

--- Soupservings.java
+++ Soupservings.java
@@ -28,3 +28,32 @@
      * * Optimal Solution:
      */
     
+class Solution {
+    public double soupServings(int n) {
+        if (n >= 4800) 
+        return 1.0;
+        return helper((n + 24) / 25, (n + 24) / 25, new Double[200][200]);
+    }
+
+    private double helper(int a, int b, Double[][] memo) {
+        if (a <= 0 && b <= 0)
+         return 0.5;
+
+        if (a <= 0) 
+        return 1.0;
+
+        if (b <= 0) 
+        return 0.0;
+
+        if (memo[a][b] != null) 
+        return memo[a][b];
+        
+        memo[a][b] = 0.25 * (
+            helper(a - 4, b, memo) +
+            helper(a - 3, b - 1, memo) +
+            helper(a - 2, b - 2, memo) +
+            helper(a - 1, b - 3, memo)
+        );
+        return memo[a][b];
+    }
+}
