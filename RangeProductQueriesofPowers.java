487e8243f7ae182882d6fc5a63c54a13be54bb12
riyatyag <tyagiriya517@gmail.com>
2025-08-11 11:12:48 +0530

    Day 66

--- RangeProductQueriesofPowers.java
+++ RangeProductQueriesofPowers.java
@@ -25,3 +25,54 @@
 // - The `powers` array will have at most log n elements.
 // - The `prefixProducts` array will also have at most log n elements.
 
+class Solution {
+    static final int MOD = 1_000_000_007;
+
+    public int[] productQueries(int n, int[][] queries) {
+        // Step 1: Extract powers of 2 from n
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 31; i++) {
+            if ((n & (1 << i)) != 0) {
+                list.add(1 << i);
+            }
+        }
+
+        int m = list.size();
+        long[] prefix = new long[m];
+        long[] invPrefix = new long[m];
+
+        // Step 2: Build prefix product and its inverses
+        prefix[0] = list.get(0) % MOD;
+        for (int i = 1; i < m; i++) {
+            prefix[i] = (prefix[i - 1] * list.get(i)) % MOD;
+        }
+
+        // Precompute inverses
+        for (int i = 0; i < m; i++) {
+            invPrefix[i] = modPow(prefix[i], MOD - 2);
+        }
+
+        // Step 3: Answer queries
+        int[] ans = new int[queries.length];
+        for (int q = 0; q < queries.length; q++) {
+            int l = queries[q][0], r = queries[q][1];
+            if (l == 0) {
+                ans[q] = (int) prefix[r];
+            } else {
+                ans[q] = (int) ((prefix[r] * invPrefix[l - 1]) % MOD);
+            }
+        }
+
+        return ans;
+    }
+
+    private long modPow(long base, long exp) {
+        long res = 1;
+        while (exp > 0) {
+            if ((exp & 1) == 1) res = (res * base) % MOD;
+            base = (base * base) % MOD;
+            exp >>= 1;
+        }
+        return res;
+    }
+}
