4635dda641b1189747de6dc50bf661d2d6c47840
riyatyag <tyagiriya517@gmail.com>
2025-08-15 10:42:48 +0530

    Day 70

--- Insertinterval.java
+++ Insertinterval.java
@@ -37,6 +37,28 @@
  * We create a new list to store the result, which in the worst case could contain all N original intervals plus the new one.
  */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
+class Solution {
+    public ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0, n = intervals.length;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
+
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result;
+    }
+}
