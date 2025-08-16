f517696aaace1488c3215ddb772b5deeb2ef0cda
riyatyag <tyagiriya517@gmail.com>
2025-08-16 11:50:38 +0530

    Day 71

--- Maximum69number.java
+++ Maximum69number.java
@@ -45,3 +45,17 @@
      * The space complexity is determined by the storage required for the string or character array representation of `num`, which is proportional to the number of digits.
      */
 
+
+class Solution {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9'; // change the first '6' to '9'
+                break; // only one change allowed
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
