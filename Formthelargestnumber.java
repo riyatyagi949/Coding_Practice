d5ee3c15c8e90f87be29dd1cf8afdc908198b168
riyatyag <tyagiriya517@gmail.com>
2025-08-16 11:45:29 +0530

    Day 71

--- Formthelargestnumber.java
+++ Formthelargestnumber.java
@@ -19,4 +19,22 @@
      * Space Complexity: O(N * K) to store the string representations of the numbers.
      */
 
-    
+    class Solution {
+    public String findLargest(int[] arr) {
+        String[] strArr = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            strArr[i] = String.valueOf(arr[i]);
+        }
+        Arrays.sort(strArr, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strArr[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        for (String s : strArr) {
+            result.append(s);
+        }
+
+        return result.toString();
+    }
+}
