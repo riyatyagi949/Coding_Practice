e6104895663cd6c11f5c26d2d04cfcd75ad7304c
riyatyag <tyagiriya517@gmail.com>
2025-08-21 10:17:16 +0530

    Day 76

--- Maximumtheminimumdifferencebetweenkelements.java
+++ Maximumtheminimumdifferencebetweenkelements.java
@@ -16,3 +16,39 @@
 // The space complexity is O(1) if the array is sorted in-place.
 
 // Optimal  Solution - 
+
+class Solution {
+    public int maxMinDiff(int[] arr, int k) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        
+        int low = 0, high = arr[n - 1] - arr[0];
+        int ans = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canPlace(arr, k, mid)) {
+                ans = mid;
+                low = mid + 1;  
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+    private boolean canPlace(int[] arr, int k, int mid) {
+        int count = 1; 
+        int lastPicked = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - lastPicked >= mid) {
+                count++;
+                lastPicked = arr[i];
+                if (count >= k)
+                return true;
+            }
+        }
+        return false;
+    }
+}
