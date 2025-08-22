6d97ccb5f7824d68d7cbac30a244bd18e9dc0f85
riyatyag <tyagiriya517@gmail.com>
2025-08-22 10:53:34 +0530

    Day 77

--- Medianinarow.java
+++ Medianinarow.java
@@ -25,3 +25,42 @@
 // The algorithm uses a constant amount of extra space, so the space complexity is O(1).
 
 // Optimal Solution:
+class Solution {
+    public int median(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+
+        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            low = Math.min(low, mat[i][0]);     
+            high = Math.max(high, mat[i][m - 1]); 
+        }
+        int required = (n * m + 1) / 2;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                count += countLessEqual(mat[i], mid);
+            }
+
+            if (count < required) {
+                low = mid + 1;
+            }
+            else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+    private int countLessEqual(int[] row, int target) {
+        int l = 0, r = row.length;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (row[mid] <= target) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
+
