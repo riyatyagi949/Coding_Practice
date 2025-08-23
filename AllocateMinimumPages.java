805cc7414f43bdc9ecf82753b021ffd58a46c289
riyatyag <tyagiriya517@gmail.com>
2025-08-23 10:48:30 +0530

    Day 78

--- AllocateMinimumPages.java
+++ AllocateMinimumPages.java
@@ -43,3 +43,46 @@
 
 
 // Optimal Solution - 
+class Solution {
+    public int findPages(int[] arr, int k) {
+        int n = arr.length;
+        if (k > n) 
+        return -1; 
+
+        int low = 0, high = 0;
+        for (int pages : arr) {
+            low = Math.max(low, pages); 
+            high += pages;           
+        }
+
+        int ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (isPossible(arr, n, k, mid)) {
+                ans = mid;
+                high = mid - 1; 
+            } 
+            else {
+                low = mid + 1; 
+            }
+        }
+        return ans;
+    }
+
+    private boolean isPossible(int[] arr, int n, int k, int mid) {
+        int students = 1, pages = 0;
+        for (int i = 0; i < n; i++) {
+            if (pages + arr[i] > mid) {
+                students++;
+                pages = arr[i];
+                
+                if (students > k) 
+                return false;
+            }
+            else {
+                pages += arr[i];
+            }
+        }
+        return true;
+    }
+}
