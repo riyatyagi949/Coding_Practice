3ec3c04e82eebb4eb741a85412198d701ef97c8d
riyatyag <tyagiriya517@gmail.com>
2025-08-24 10:41:33 +0530

    Day 79

--- Longestsubarrayof1'safterdeletingoneelement.java
+++ Longestsubarrayof1'safterdeletingoneelement.java
@@ -63,3 +63,18 @@
 //  *
 
 // Optimal Solution - 
+class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, zeros = 0, max = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0) zeros++;
+            while (zeros > 1) {
+                if (nums[left] == 0) zeros--;
+                left++;
+            }
+            max = Math.max(max, right - left);
+        }
+        return max;
+    }
+}
