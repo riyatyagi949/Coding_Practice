b6632a6de3f1be94242b02b4a6ad6d8f56e1f445
riyatyag <tyagiriya517@gmail.com>
2025-08-25 11:02:08 +0530

    Day 80

--- MaximizemedianafterdoingKadditionoperation.java
+++ MaximizemedianafterdoingKadditionoperation.java
@@ -114,3 +114,65 @@
 // Space Complexity: O(1) if we modify the input array, or O(N) if we create a new sorted array.
 
 // Optimal  Solution - 
+
+import java.util.Arrays;
+
+class Solution {
+    static boolean isPossible(int[] arr, int target, int k) {
+        int n = arr.length;
+
+        if (n % 2 == 1) {
+         for (int i = n / 2; i < n; ++i) {
+                if (arr[i] < target)
+                    k -= (target - arr[i]);
+            }
+        }
+        else {
+           if (arr[n / 2] <= target) {
+                k -= (target - arr[n / 2]);
+                k -= (target - arr[n / 2 - 1]);
+            }
+            else {
+                k -= (2 * target - (arr[n / 2] + arr[n / 2 - 1]));
+            }
+            for (int i = n / 2 + 1; i < n; ++i) {
+                if (arr[i] < target)
+                    k -= (target - arr[i]);
+            }
+        }
+        return k >= 0;
+    }
+    static int maximizeMedian(int[] arr, int k) {
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int iniMedian = arr[n / 2];
+        if (n % 2 == 0) {
+            iniMedian += arr[n / 2 - 1];
+            iniMedian /= 2;
+        }
+
+        int low = iniMedian, high = iniMedian + k;
+        int bestMedian = iniMedian;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (isPossible(arr, mid, k)) {
+                bestMedian = mid;
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+        return bestMedian;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 5};
+        int k = 3;
+        int result = maximizeMedian(arr, k);
+        System.out.println(result);
+    }
+}
