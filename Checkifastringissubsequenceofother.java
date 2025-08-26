011a9b1fe1c250a0d8896fee128a64e38ba9f17b
riyatyag <tyagiriya517@gmail.com>
2025-08-26 10:37:03 +0530

    Day 81

--- Checkifastringissubsequenceofother.java
+++ Checkifastringissubsequenceofother.java
@@ -20,7 +20,6 @@ class Solution {
     public boolean isSubSequence(String s1, String s2) {
         int i = 0; 
         int j = 0; 
-        // pointer for s2
 
         while (i < s1.length() && j < s2.length()) {
             if (s1.charAt(i) == s2.charAt(j)) {
