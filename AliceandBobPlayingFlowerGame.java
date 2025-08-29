63da14c9f924c9fef3a9b3d18f846d901aa2b306
riyatyag <tyagiriya517@gmail.com>
2025-08-29 10:10:42 +0530

    Day 84

--- AliceandBobPlayingFlowerGame.java
+++ AliceandBobPlayingFlowerGame.java
@@ -41,6 +41,8 @@
  * Space Complexity: O(1)
  * No extra space is required to store data.
  */
+
+// Optimal  Solution -
 class Solution {
     public long flowerGame(int n, int m) {
         long odd_n = (n + 1) / 2;
