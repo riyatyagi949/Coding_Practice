e513400ec3cbd21700ccad6558edd7de9ad9dbac
riyatyag <tyagiriya517@gmail.com>
2025-08-30 10:51:30 +0530

    Day 85

--- TheCelebrityProblem.java
+++ TheCelebrityProblem.java
@@ -30,4 +30,24 @@
 // Space Complexity: O(1), as we are only using a few pointers and no additional data structures.
 
 // Optimal Solution:
+class Solution {
+    public int celebrity(int mat[][]) {
+        int n = mat.length;
+        
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (mat[candidate][i] == 1) {
+                candidate = i;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (i != candidate) {
+                if (mat[candidate][i] == 1 || mat[i][candidate] == 0) {
+                    return -1;
+                }
+            }
+        }
+        return candidate;
+    }
+}
 
