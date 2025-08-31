16622d860584df2017c6c4a47a5e9d15d1b8e85a
riyatyag <tyagiriya517@gmail.com>
2025-08-31 10:54:59 +0530

    Day 86

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -29,18 +29,24 @@ class Solution {
     public void solveSudoku(char[][] board) {
         solve(board);
     }
-
     private boolean solve(char[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    for (char c = '1'; c <= '9'; c++) {
-                        if (isValid(board, i, j, c)) {
-                            board[i][j] = c;
-                            if (solve(board)) {
-                                return true;
-                            } else {
-                                board[i][j] = '.'; // Backtrack
+    for (int i = 0; i < 9; i++)
+     {
+         for (int j = 0; j < 9; j++)
+          {
+           if (board[i][j] == '.') 
+           {
+         for (char c = '1'; c <= '9'; c++)
+          {
+             if (isValid(board, i, j, c))
+              {
+                 board[i][j] = c;
+                 if (solve(board))
+                 {
+                    return true;
+                            } 
+                else {
+                    board[i][j] = '.'; 
                             }
                         }
                     }
