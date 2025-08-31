a5131c80e35cf659263295ee782c867de93dc6b0
riyatyag <tyagiriya517@gmail.com>
2025-08-31 10:55:43 +0530

    Day 86

--- SudokuSolver.java
+++ SudokuSolver.java
@@ -58,10 +58,16 @@ class Solution {
     }
 
     private boolean isValid(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < 9; i++) {
-            if (board[i][col] == c) return false;
-            if (board[row][i] == c) return false;
-            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        for (int i = 0; i < 9; i++) 
+        {
+            if (board[i][col] == c)
+             return false;
+
+            if (board[row][i] == c) 
+            return false;
+            
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+             return false;
         }
         return true;
     }
