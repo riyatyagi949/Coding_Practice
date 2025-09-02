313eb5c343d9bc5511ce72c297ebcb5fcdcd1e45
riyatyag <tyagiriya517@gmail.com>
2025-09-02 19:00:33 +0530

    Day 88

--- SwapKthNodesfromEnds.java
+++ SwapKthNodesfromEnds.java
@@ -30,3 +30,56 @@ O(1), as we only use a few extra pointers.
 
 Optimal Solution:
 */
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public Node swapKth(Node head, int k) {
+        if (head == null)
+        return head;
+        
+        int n = 0;
+        Node temp = head;
+        while (temp != null) {
+            n++;
+            temp = temp.next;
+        }
+        if (k > n) 
+        return head;
+
+        if (2 * k - 1 == n) 
+        return head;
+
+        Node xPrev = null, x = head;
+        for (int i = 1; i < k; i++) {
+            xPrev = x;
+            x = x.next;
+        }
+        Node yPrev = null, y = head;
+        for (int i = 1; i < n - k + 1; i++) {
+            yPrev = y;
+            y = y.next;
+        }
+        if (xPrev != null)
+        xPrev.next = y;
+        if (yPrev != null) 
+        yPrev.next = x;
+
+        Node tempNext = x.next;
+        x.next = y.next;
+        y.next = tempNext;
+
+        if (k == 1) head = y;
+        if (k == n) head = x;
+
+        return head;
+    }
+}
