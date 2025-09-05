dd4e624533d798281987892cb4e4c5429a939de5
riyatyag <tyagiriya517@gmail.com>
2025-09-05 10:46:03 +0530

    Day 91

--- SortaLinkedListof0s,1sand2s.java
+++ SortaLinkedListof0s,1sand2s.java
@@ -26,3 +26,46 @@ Time and Space Complexity:
 
 Optimal Solution in Java:
 */
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+class Solution {
+    public Node segregate(Node head) {
+        if (head == null || head.next == null) return head;
+
+        Node zeroHead = new Node(-1), oneHead = new Node(-1), twoHead = new Node(-1);
+        Node zero = zeroHead, one = oneHead, two = twoHead;
+
+        Node curr = head;
+        while (curr != null) {
+            if (curr.data == 0) {
+                zero.next = curr;
+                zero = zero.next;
+            } 
+            else if (curr.data == 1) {
+                one.next = curr;
+                one = one.next;
+            } 
+            else {
+                two.next = curr;
+                two = two.next;
+            }
+            curr = curr.next;
+        }
+
+        zero.next = (oneHead.next != null) ? oneHead.next : twoHead.next;
+        one.next = twoHead.next;
+        two.next = null;
+
+        return zeroHead.next;
+    }
+}
+
