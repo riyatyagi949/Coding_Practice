896d3292d4e018b9e3aba43cf2cccb718c7a14f3
riyatyag <tyagiriya517@gmail.com>
2025-09-06 10:43:43 +0530

    Day 92

--- FindtheLengthofLoop.java
+++ FindtheLengthofLoop.java
@@ -21,3 +21,42 @@
 
 // Optimal Solution:
 
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+        if (head == null)
+        return 0;
+
+        Node slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return countLoopLength(slow);
+            }
+        }
+        return 0; 
+    }
+    private int countLoopLength(Node nodeInLoop) {
+        int count = 1;
+        Node temp = nodeInLoop.next;
+
+        while (temp != nodeInLoop) {
+            count++;
+            temp = temp.next;
+        }
+
+        return count;
+    }
+}
