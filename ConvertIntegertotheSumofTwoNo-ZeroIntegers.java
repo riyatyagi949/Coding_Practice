563c7517cc6443dde845ec69be690ba8d1a4ec26
riyatyag <tyagiriya517@gmail.com>
2025-09-08 10:48:22 +0530

    Day 93

--- ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
+++ ConvertIntegertotheSumofTwoNo-ZeroIntegers.java
@@ -29,3 +29,22 @@
 // The space used does not depend on the input size N.
 
 // Optimal  Solution in Java - 
+class Solution {
+    public int[] getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (isNoZero(a) && isNoZero(b)) {
+                return new int[]{a, b};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    private boolean isNoZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) 
+            return false;
+            num /= 10;
+        }
+        return true;
+    }
+}
