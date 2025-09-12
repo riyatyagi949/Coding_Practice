95cacf7ded78d6e3a19276ae2860cc1b99be6060
riyatyag <tyagiriya517@gmail.com>
2025-09-12 11:08:58 +0530

    Day 97

--- MinimizetheHeightsII.java
+++ MinimizetheHeightsII.java
@@ -39,5 +39,27 @@
  */
 
 //  Optimal  Solution in Java-
-import java.util.Arrays;
+import java.util.*;
+
+class Solution {
+    public int getMinDiff(int[] arr, int k) {
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        int ans = arr[n-1] - arr[0];
+        int smallest = arr[0] + k;
+        int largest = arr[n-1] - k;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minVal = Math.min(smallest, arr[i+1] - k);
+            int maxVal = Math.max(largest, arr[i] + k);
+
+            if (minVal < 0) 
+            continue; 
+            ans = Math.min(ans, maxVal - minVal);
+        }
+
+        return ans;
+    }
+}
 
