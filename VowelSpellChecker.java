ed86178fa0c1c93a0dcbf1fc1ade6c3136a3b7b4
riyatyag <tyagiriya517@gmail.com>
2025-09-14 15:00:19 +0530

    Day 99

--- VowelSpellChecker.java
+++ VowelSpellChecker.java
@@ -41,3 +41,46 @@
  *
  * Space Complexity: O(L) to store the three hash maps, which on average store all the words from the `wordlist`.
  */
+
+//  Optimal Solution in Java - 
+
+class Solution {
+    public String[] spellchecker(String[] wordlist, String[] queries) {
+        Set<String> exactWords = new HashSet<>();
+        Map<String, String> caseInsensitive = new HashMap<>();
+        Map<String, String> vowelInsensitive = new HashMap<>();
+        
+        for (String word : wordlist) {
+            exactWords.add(word);
+            
+            String lower = word.toLowerCase();
+            caseInsensitive.putIfAbsent(lower, word);
+            
+            String devoweled = devowel(lower);
+            vowelInsensitive.putIfAbsent(devoweled, word);
+        }
+        
+        String[] result = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            String query = queries[i];
+            
+            if (exactWords.contains(query)) {
+                result[i] = query;
+            }
+             else {
+                String lower = query.toLowerCase();
+                if (caseInsensitive.containsKey(lower)) {
+                    result[i] = caseInsensitive.get(lower);
+                }
+                 else {
+                    String devoweled = devowel(lower);
+                    result[i] = vowelInsensitive.getOrDefault(devoweled, "");
+                }
+            }
+        }
+        return result;
+    }
+    private String devowel(String word) {
+        return word.replaceAll("[aeiou]", "*");
+    }
+}
