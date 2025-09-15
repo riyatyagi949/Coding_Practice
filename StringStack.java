c54368b5b8dfa87b624378cfc6073aa7a90bf682
riyatyag <tyagiriya517@gmail.com>
2025-09-15 12:37:28 +0530

    Day 100

--- StringStack.java
+++ StringStack.java
@@ -166,3 +166,20 @@
  * Space Complexity: O(1). We use a constant amount of extra space for pointers and counters.
  */
 // Optimal Solution in Java-
+class Solution {
+    public boolean stringStack(String pat, String tar) {
+        int i = pat.length() - 1; 
+        int j = tar.length() - 1;  
+
+        while (i >= 0 && j >= 0) {
+            if (pat.charAt(i) == tar.charAt(j)) {
+                i--;
+                j--;
+            } 
+            else {
+                i -= 2;
+            }
+        }
+         return j < 0;
+    }
+}
