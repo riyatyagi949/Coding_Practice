f3c1e0aef04ccb386413bacec52b9b7be5cedd38
riyatyag <tyagiriya517@gmail.com>
2025-09-16 11:18:59 +0530

    Day 101

--- PostfixEvaluation.java
+++ PostfixEvaluation.java
@@ -33,3 +33,46 @@
 
 import java.util.Stack;
 
+class Solution {
+    public int evaluatePostfix(String[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : arr) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = 0;
+                
+                switch (token) {
+                    case "+":
+                        result = a + b;
+                        break;
+                    case "-":
+                        result = a - b;
+                        break;
+                    case "*":
+                        result = a * b;
+                        break;
+                    case "/":
+                        result = Math.floorDiv(a, b); 
+                        break;
+                    case "^":
+                        result = (int)Math.pow(a, b);
+                        break;
+                }
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+    
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || 
+               token.equals("*") || token.equals("/") || 
+               token.equals("^");
+    }
+}
+
+
