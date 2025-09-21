b708eb7f29799c2ce3c36368528b8d352c473518
riyatyag <tyagiriya517@gmail.com>
2025-09-21 10:10:33 +0530

    Day 107

--- MaxRectangle.java
+++ MaxRectangle.java
@@ -30,3 +30,42 @@
 
 import java.util.Stack;
 
+class Solution {
+    static int maxArea(int mat[][]) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[] height = new int[m];
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0)
+                    height[j] = 0;
+                else
+                    height[j] += 1;
+            }
+            maxArea = Math.max(maxArea, largestRectangleArea(height));
+        }
+        return maxArea;
+    }
+    private static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            int h = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && h < heights[stack.peek()])
+             {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : (i - stack.peek() - 1);
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
+
+
