eb63dafb2de55c7eeefb9c8b9bc09949b0e982c7
riyatyag <tyagiriya517@gmail.com>
2025-09-26 10:38:04 +0530

    Day 112

--- ValidTriangleNumber.java
+++ ValidTriangleNumber.java
@@ -39,3 +39,27 @@
  * Space Complexity: O(1) if sorting is done in-place, or $O(\log n)$ to $O(n)$ depending on the space complexity of the sorting algorithm used by the language implementation.
  */
 // Optimal Solution  in Java - 
+class Solution {
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int count = 0;
+
+        for (int k = n - 1; k >= 2; k--)
+         {
+            int i = 0, j = k - 1;
+            while (i < j)
+             {
+                if (nums[i] + nums[j] > nums[k])
+                 {
+                    count += (j - i);
+                    j--;
+                } 
+                else {
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}
