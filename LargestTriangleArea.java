35e2b3f33e0277aa776e133a0acdad1d5fd51121
riyatyag <tyagiriya517@gmail.com>
2025-09-27 20:59:17 +0530

    Day 113

--- LargestTriangleArea.java
+++ LargestTriangleArea.java
@@ -27,3 +27,23 @@
  * Space Complexity: O(1). We use only a few constant-size variables for area calculation and iteration.
  */
 // Optimal  Solution in Java -
+class Solution {
+    public double largestTriangleArea(int[][] points) {
+        double maxArea = 0;
+        int n = points.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    double area = Math.abs(
+                        points[i][0] * (points[j][1] - points[k][1]) +
+                        points[j][0] * (points[k][1] - points[i][1]) +
+                        points[k][0] * (points[i][1] - points[j][1])
+                    ) / 2.0;
+                     maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+}
