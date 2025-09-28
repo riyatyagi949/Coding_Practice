d7ba016d4276f4e3e2cb5e9e897a905f352081ce
riyatyag <tyagiriya517@gmail.com>
2025-09-28 10:24:45 +0530

    Day 114

--- LargestPerimeterTriangle.java
+++ LargestPerimeterTriangle.java
@@ -23,3 +23,17 @@
  */
 // Optimal  Solution in Java - 
 import java.util.*;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums); 
+        for (int i = nums.length - 1; i >= 2; i--)
+         {
+            if (nums[i - 2] + nums[i - 1] > nums[i])
+             {
+                return nums[i] + nums[i - 1] + nums[i - 2];
+            }
+        }
+        return 0;
+    }
+}
