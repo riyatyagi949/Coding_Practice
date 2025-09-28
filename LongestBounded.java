0d4f3d533fb6c4ec5e79b1030bf17d8b521f0133
riyatyag <tyagiriya517@gmail.com>
2025-09-28 10:21:09 +0530

    Day 114

--- LongestBounded.java
+++ LongestBounded.java
@@ -34,3 +34,42 @@
 
 import java.util.*;
 
+class Solution {
+    public ArrayList<Integer> longestSubarray(int[] arr, int x) {
+        int n = arr.length;
+        Deque<Integer> maxDeque = new ArrayDeque<>();
+        Deque<Integer> minDeque = new ArrayDeque<>();
+
+        int left = 0, bestStart = 0, bestLen = 0;
+
+        for (int right = 0; right < n; right++) {
+            while (!maxDeque.isEmpty() && arr[maxDeque.peekLast()] < arr[right]) {
+                maxDeque.pollLast();
+            }
+            maxDeque.addLast(right);
+
+            while (!minDeque.isEmpty() && arr[minDeque.peekLast()] > arr[right]) {
+                minDeque.pollLast();
+            }
+            minDeque.addLast(right);
+
+            while (arr[maxDeque.peekFirst()] - arr[minDeque.peekFirst()] > x) {
+                left++;
+                if (maxDeque.peekFirst() < left) maxDeque.pollFirst();
+                if (minDeque.peekFirst() < left) minDeque.pollFirst();
+            }
+            if (right - left + 1 > bestLen) {
+                bestLen = right - left + 1;
+                bestStart = left;
+            }
+        }
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (int i = bestStart; i < bestStart + bestLen; i++) {
+            ans.add(arr[i]);
+        }
+        return ans;
+    }
+}
+
+
