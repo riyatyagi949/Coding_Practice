18464c4bb0355181c4fb198b0e6a1438a9ed4f72
riyatyag <tyagiriya517@gmail.com>
2025-09-30 14:14:11 +0530

    Day 116

--- FindTriangularSumofanArray.java
+++ FindTriangularSumofanArray.java
@@ -20,3 +20,15 @@
  * Space Complexity: O(1) (or O(n) depending on how the input is handled). Since we modify the input array in place, we use no extra space proportional to $n$ for new arrays, resulting in $O(1)$ auxiliary space.
  */
 // Optimal Solution in Java -
+
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        for (int size = n; size > 1; size--) {
+            for (int i = 0; i < size - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+        return nums[0];
+    }
+}
