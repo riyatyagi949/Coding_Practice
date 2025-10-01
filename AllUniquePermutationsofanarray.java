5eecabf97500244f92e245f9d420d35fd3716f48
riyatyag <tyagiriya517@gmail.com>
2025-10-01 10:13:47 +0530

    Day 117

--- AllUniquePermutationsofanarray.java
+++ AllUniquePermutationsofanarray.java
@@ -37,5 +37,37 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> uniquePerms(int[] arr) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        Arrays.sort(arr); 
+        boolean[] used = new boolean[arr.length];
+        backtrack(arr, used, new ArrayList<>(), result);
+        return result;
+    }
+    
+    private static void backtrack(int[] arr, boolean[] used, 
+                                  ArrayList<Integer> current, 
+                                  ArrayList<ArrayList<Integer>> result) {
+        if (current.size() == arr.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+    for (int i = 0; i < arr.length; i++) {
+            if (used[i]) 
+            continue;
+            
+            if (i > 0 && arr[i] == arr[i - 1] && !used[i - 1]) 
+            continue;
+            
+            used[i] = true;
+            current.add(arr[i]);
+            
+            backtrack(arr, used, current, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
 
