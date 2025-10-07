eddf4895eac78332e5d442d9fb2bfae7c7a3eeaf
riyatyag <tyagiriya517@gmail.com>
2025-10-07 10:36:58 +0530

    Day 122

--- AvoidFloodintheCity.java
+++ AvoidFloodintheCity.java
@@ -52,3 +52,34 @@
  * - The `dryDays` TreeSet can store up to $n$ indices in the worst case (if half the days are dry).
  */
 // Optimal Solution in Java -
+import java.util.*;
+
+class Solution {
+    public int[] avoidFlood(int[] rains) {
+        int n = rains.length;
+        int[] ans = new int[n];
+        Map<Integer, Integer> full = new HashMap<>();
+        TreeSet<Integer> dryDays = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            if (rains[i] == 0) 
+            {
+                dryDays.add(i);
+                ans[i] = 1;
+            }
+             else {
+                ans[i] = -1;
+                if (full.containsKey(rains[i]))
+                 {
+                    Integer dry = dryDays.higher(full.get(rains[i]));
+                    if (dry == null)
+                     return new int[0];
+                    ans[dry] = rains[i];
+                    dryDays.remove(dry);
+                }
+                full.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
