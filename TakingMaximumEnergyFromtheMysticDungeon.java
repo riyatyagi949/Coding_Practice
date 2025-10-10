96a0de4488ae18be45dbcf3cfd1bb0195f3fbce5
riyatyag <tyagiriya517@gmail.com>
2025-10-10 18:11:46 +0530

    Day 124

--- TakingMaximumEnergyFromtheMysticDungeon.java
+++ TakingMaximumEnergyFromtheMysticDungeon.java
@@ -40,6 +40,7 @@ class Solution {
     int[] dp = energy.clone();
     for (int i = energy.length - 1 - k; i >= 0; --i)
       dp[i] += dp[i + k];
+      
     return Arrays.stream(dp).max().getAsInt();
   }
 }
