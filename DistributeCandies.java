f0b616608364ebeccac24ee1eeb2620cbc766c17
riyatyag <tyagiriya517@gmail.com>
2025-10-12 12:30:59 +0530

    Day 126

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -34,3 +34,33 @@
  * Space Complexity: O(h), where `h` is the height of the binary tree, due to the recursion stack. In the worst case (skewed tree), $O(n)$.
  */
 // Optimal Solution in Java -
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+    int moves = 0;
+
+    public int distCandy(Node root) {
+        dfs(root);
+        return moves;
+    }
+     private int dfs(Node root) {
+        if (root == null)
+        return 0;
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+        return root.data + left + right - 1;
+    }
+}
