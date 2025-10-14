9333607f5faf481e8bf7b8cf323194c77ba754a3
riyatyag <tyagiriya517@gmail.com>
2025-10-14 11:44:41 +0530

    Day 127

--- SumofNodesinBSTRange.java
+++ SumofNodesinBSTRange.java
@@ -51,3 +51,27 @@
  * - In a highly skewed BST (worst case), H = O(N).
  */
 // Optimal Solution - 
+
+class Node {
+    int data;
+    Node left, right;
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+class Solution {
+    public int nodeSum(Node root, int l, int r) {
+        if (root == null) 
+        return 0;
+        
+        if (root.data < l)
+            return nodeSum(root.right, l, r);
+        
+        if (root.data > r)
+            return nodeSum(root.left, l, r);
+        
+        return root.data + nodeSum(root.left, l, r) + nodeSum(root.right, l, r);
+    }
+}
+
