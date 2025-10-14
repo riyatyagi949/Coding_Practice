c9c55d1e1696649d5de730b308afdbd3a7ed17d9
riyatyag <tyagiriya517@gmail.com>
2025-10-14 11:50:44 +0530

    Day 127

--- AdjacentIncreasingSubarraysDetectionI.java
+++ AdjacentIncreasingSubarraysDetectionI.java
@@ -54,3 +54,29 @@
  * allocate any additional data structures whose size depends on N or K.
  */
 // Optimal Solution in Java - 
+import java.util.*;
+
+class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        
+        for (int i = 0; i + 2 * k <= n; i++)
+         {
+            if (isIncreasing(nums, i, i + k) && isIncreasing(nums, i + k, i + 2 * k))
+             {
+                return true;
+            }
+        }
+        return false;
+    }
+    private boolean isIncreasing(List<Integer> nums, int start, int end) {
+        for (int i = start + 1; i < end; i++)
+         {
+            if (nums.get(i) <= nums.get(i - 1)) 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
