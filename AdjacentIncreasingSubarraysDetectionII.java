5e3d3754a9e4659b3cb0b43211946649e790a4b5
riyatyag <tyagiriya517@gmail.com>
2025-10-15 13:11:07 +0530

    Day 128

--- AdjacentIncreasingSubarraysDetectionII.java
+++ AdjacentIncreasingSubarraysDetectionII.java
@@ -71,3 +71,32 @@
  * - This gives a space complexity of O(N) + O(N) = O(N).
  */
 // Optimal Solution in Java-
+import java.util.*;
+
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int n = nums.size();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums.get(i) > nums.get(i - 1))
+                left[i] = left[i - 1] + 1;
+            else
+                left[i] = 1;
+        }
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums.get(i) < nums.get(i + 1))
+                right[i] = right[i + 1] + 1;
+            else
+                right[i] = 1;
+        }
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans = Math.max(ans, Math.min(left[i], right[i + 1]));
+        }
+        return ans;
+    }
+}
