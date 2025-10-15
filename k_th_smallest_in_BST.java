11bf98807de261b1cea0417190277b3da02a3a8b
riyatyag <tyagiriya517@gmail.com>
2025-10-15 13:06:30 +0530

    Day 128

--- k_th_smallest_in_BST.java
+++ k_th_smallest_in_BST.java
@@ -45,3 +45,37 @@
  * - In a skewed BST, O(N).
  */
 // Optimal Solution in Java -
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int d)
+    {
+        data = d;
+        left = right = null;
+     }
+ }
+
+class Solution {
+    int count = 0;   
+    int result = -1;
+
+    public int kthSmallest(Node root, int k) {
+        inorder(root, k);
+        return result;
+    }
+   private void inorder(Node root, int k) {
+        if (root == null) 
+        return;
+
+        inorder(root.left, k);
+
+        count++;
+        if (count == k) {
+            result = root.data;
+            return; 
+        }
+         inorder(root.right, k);
+    }
+}
