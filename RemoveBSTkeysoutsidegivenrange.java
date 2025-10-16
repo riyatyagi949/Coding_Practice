51914ddf98b675078e713b659edc5463215c45a6
riyatyag <tyagiriya517@gmail.com>
2025-10-16 18:48:21 +0530

    Day 129

--- RemoveBSTkeysoutsidegivenrange.java
+++ RemoveBSTkeysoutsidegivenrange.java
@@ -60,3 +60,30 @@
  * - In a highly skewed BST (worst case), H = O(N).
  */
 // Optimal Solution in Java - 
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+class Solution {
+    Node removekeys(Node root, int l, int r) {
+        if (root == null) 
+            return null;
+        
+        root.left = removekeys(root.left, l, r);
+        root.right = removekeys(root.right, l, r);
+        
+        if (root.data < l)
+            return root.right;
+        if (root.data > r)
+            return root.left;
+            
+            return root;
+    }
+}
