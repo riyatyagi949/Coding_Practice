4824adb55622123256c3f8314ed11b3c27e68280
riyatyag <tyagiriya517@gmail.com>
2025-10-22 11:07:03 +0530

    Day 134

--- NearlySorted.java
+++ NearlySorted.java
@@ -55,3 +55,23 @@
  */ 
 // Optimal Solution in Java -
 import java.util.*;
+
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        
+        int n = arr.length;
+        int index = 0;
+
+        for (int i = 0; i < Math.min(k + 1, n); i++) {
+            minHeap.add(arr[i]);
+        }
+        for (int i = k + 1; i < n; i++) {
+            arr[index++] = minHeap.poll();
+            minHeap.add(arr[i]);
+        }
+        while (!minHeap.isEmpty()) {
+            arr[index++] = minHeap.poll();
+        }
+    }
+}
