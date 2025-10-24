e010eaefe8a45acb5e387576a96f6935db267794
riyatyag <tyagiriya517@gmail.com>
2025-10-24 11:16:06 +0530

    Day 136

--- NextGreaterNumericallyBalancedNumber.java
+++ NextGreaterNumericallyBalancedNumber.java
@@ -24,3 +24,60 @@
  */
 // Optimal Solution in Java-
 import java.util.*;
+
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        int maxLen = 7;
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        for (int mask = 1; mask < (1 << 9); mask++) {
+            int len = 0;
+            for (int b = 0; b < 9; b++) {
+                if ((mask & (1 << b)) != 0) {
+                    len += (b + 1);
+                }
+            }
+            if (len == 0 || len > maxLen) continue;
+            
+            char[] arr = new char[len];
+            int idx = 0;
+            for (int b = 0; b < 9; b++) {
+                if ((mask & (1 << b)) != 0) {
+                    char ch = (char)('0' + (b + 1));
+                    int repeat = b + 1;
+                    for (int r = 0; r < repeat; r++) {
+                        arr[idx++] = ch;
+                    }
+                }
+            }
+            Arrays.sort(arr);
+            do {
+                if (arr[0] == '0') continue; 
+                int val = 0;
+                for (char c : arr) val = val * 10 + (c - '0');
+                set.add(val);
+            }
+             while (nextPermutation(arr));
+        }
+            Integer ans = set.higher(n);
+            return ans == null ? -1 : ans;
+    }
+    private boolean nextPermutation(char[] a) {
+        int i = a.length - 2;
+        while (i >= 0 && a[i] >= a[i + 1]) i--;
+        if (i < 0)
+         return false;
+         
+        int j = a.length - 1;
+        while (a[j] <= a[i]) j--;
+        swap(a, i, j);
+        reverse(a, i + 1, a.length - 1);
+        return true;
+    }
+    private void swap(char[] a, int i, int j) {
+        char t = a[i]; a[i] = a[j]; a[j] = t;
+    }
+    private void reverse(char[] a, int l, int r) {
+        while (l < r) swap(a, l++, r--);
+    }
+}
