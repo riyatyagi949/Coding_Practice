c9b8c66881660758e983285c9f7d44dacd6718d1
riyatyag <tyagiriya517@gmail.com>
2025-10-28 10:33:09 +0530

    Day 139

--- MakeArrayElementsEqualtoZero.java
+++ MakeArrayElementsEqualtoZero.java
@@ -72,3 +72,29 @@
  * storage is independent of the input size N.
  */
 // Optimal Solution in Java - 
+
+class Solution {
+    public int countValidSelections(int[] nums) {
+        int count = 0, len = nums.length;
+        int sum = 0;
+        for (int num : nums) 
+        sum += num;
+
+        int halfSum = 0;
+        for (int i = 0; i < len; i++) {
+            halfSum += nums[i];
+            if (nums[i] == 0)
+             {
+                if (2 * halfSum == sum)
+                 {
+                    count += 2;
+                }
+                 else if (Math.abs(sum - 2*halfSum) == 1) 
+                 {
+                    count++;
+                } 
+            }
+        }
+        return count;
+    }
+}
