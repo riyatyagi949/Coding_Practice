677f2ec52e63954c577955641965caa122d68196
riyatyag <tyagiriya517@gmail.com>
2025-10-28 10:26:44 +0530

    Day 139

--- Distanceofnearestcellhaving.java
+++ Distanceofnearestcellhaving.java
@@ -61,3 +61,52 @@
  */  
 // Optimal Solution - 
 import java.util.*;
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> nearest(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[][] dist = new int[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    dist[i][j] = 0;
+                    q.add(new int[]{i, j});
+                }
+                else {
+                    dist[i][j] = -1; 
+                }
+            }
+        }
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while (!q.isEmpty()) {
+            int[] cell = q.poll();
+            int x = cell[0], y = cell[1];
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && dist[nx][ny] == -1) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < m; j++) {
+                temp.add(dist[i][j]);
+            }
+            ans.add(temp);
+        }
+
+        return ans;
+    }
+}
