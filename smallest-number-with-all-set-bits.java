87a304ba0bd447f33925463b741a5d7b0e973498
riyatyag <tyagiriya517@gmail.com>
2025-10-29 11:00:32 +0530

    Day 140

--- smallest-number-with-all-set-bits.java
+++ smallest-number-with-all-set-bits.java
@@ -44,9 +44,11 @@
 // Optimal Solution in Java -
 
 class Solution {
-    public int smallestNumber(int n) {
+    public int smallestNumber(int n)
+     {
         int x = 1;
-        while (x < n) {
+        while (x < n)
+         {
             x = (x << 1) | 1;  
         }
         return x;
