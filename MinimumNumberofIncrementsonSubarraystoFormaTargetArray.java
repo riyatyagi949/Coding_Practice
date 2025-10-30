03ba9d64028a84f748229160d6735ca8a21e4809
riyatyag <tyagiriya517@gmail.com>
2025-10-30 12:22:19 +0530

    Day 141

--- MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
+++ MinimumNumberofIncrementsonSubarraystoFormaTargetArray.java
@@ -57,3 +57,17 @@
  * regardless of the input size.
  */
 // Optimal Solution in Java -
+class Solution {
+    public int minNumberOperations(int[] target) {
+        int operations = target[0];
+
+        for (int i = 1; i < target.length; i++)
+         {
+            if (target[i] > target[i - 1])
+             {
+                operations += target[i] - target[i - 1];
+            }
+        }
+        return operations;
+    }
+}
