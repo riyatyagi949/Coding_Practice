3761d9f82d7c289bec34e46e7b44fbb4d481cd2c
riyatyag <tyagiriya517@gmail.com>
2025-10-30 12:19:03 +0530

    Day 141

--- Replace-O's-with-X's.java
+++ Replace-O's-with-X's.java
@@ -45,3 +45,50 @@
  * - **Auxiliary Space:** No extra data structure (like a visited array) is explicitly used because the grid itself is modified to store visited states ('#'). Thus, auxiliary space is O(1), but total space complexity considering the recursion stack is O(N * M).
  */
 // Optimal Solution in Java -
+class Solution {
+    public void fill(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (grid[i][0] == 'O')
+            dfs(grid, i, 0, n, m);
+            
+            if (grid[i][m - 1] == 'O')
+            dfs(grid, i, m - 1, n, m);
+        }
+        for (int j = 0; j < m; j++) 
+        {
+            if (grid[0][j] == 'O')
+            dfs(grid, 0, j, n, m);
+            
+            if (grid[n - 1][j] == 'O')
+            dfs(grid, n - 1, j, n, m);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (grid[i][j] == 'O')
+                grid[i][j] = 'X';
+                
+                else if (grid[i][j] == 'S')
+                grid[i][j] = 'O';
+            }
+        }
+    }
+    private void dfs(char[][] grid, int i, int j, int n, int m)
+    {
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != 'O')
+        return;
+
+        grid[i][j] = 'S'; 
+
+        dfs(grid, i + 1, j, n, m);
+        dfs(grid, i - 1, j, n, m);
+        dfs(grid, i, j + 1, n, m);
+        dfs(grid, i, j - 1, n, m);
+    }
+}
+
