c6a39020634a19a62f538d65de5c8063322292c6
riyatyag <tyagiriya517@gmail.com>
2025-11-20 10:45:58 +0530

    Day 163

--- SetIntersectionSizeAtLeastTwo.java
+++ SetIntersectionSizeAtLeastTwo.java
@@ -57,3 +57,42 @@
  * in place or with minimal extra space (standard for sorting primitives).
  */
 // Optimal Solution in Java -
+import java.util.Arrays;
+
+class Solution {
+    public int intersectionSizeTwo(int[][] intervals) {
+        int n = 0;
+        long[] endStartPairs = new long[intervals.length];
+
+        for (int[] interval : intervals) {
+            endStartPairs[n] = -interval[0] & 0xFFFFFFFFL;
+            endStartPairs[n++] |= (long) (interval[1]) << 32;
+        }
+        Arrays.sort(endStartPairs);
+
+        int min = -2;
+        int max = -1;
+        int curStart;
+        int curEnd;
+        int res = 0;
+
+        for (long endStartPair : endStartPairs) {
+            curStart = -(int) endStartPair;
+            curEnd = (int) (endStartPair >> 32);
+
+            if (curStart <= min) {
+                continue;
+            }
+            if (curStart <= max) {
+                res += 1;
+                min = max;
+            }
+             else {
+                res += 2;
+                min = curEnd - 1;
+            }
+            max = curEnd;
+        }
+        return res;
+    }
+}
