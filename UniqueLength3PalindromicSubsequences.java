cc7cd6aa56167ae1fd7481efc9bce3b74d388d79
riyatyag <tyagiriya517@gmail.com>
2025-11-21 10:40:18 +0530

    Day 164

--- UniqueLength3PalindromicSubsequences.java
+++ UniqueLength3PalindromicSubsequences.java
@@ -57,3 +57,35 @@
  * - This is considered **O(1)** constant space since the alphabet size is constant.
  */
 // Optimal Solution in Java -
+import java.util.*;
+
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int[] first = new int[26];
+        int[] last = new int[26];
+        Arrays.fill(first, -1);
+        Arrays.fill(last, -1);
+
+        for (int i = 0; i < n; i++) 
+        {
+            int c = s.charAt(i) - 'a';
+            if (first[c] == -1) first[c] = i;
+            last[c] = i;
+        }
+        int count = 0;
+        for (int c = 0; c < 26; c++) {
+            if (first[c] != -1 && last[c] > first[c]) {
+                boolean[] seen = new boolean[26];
+
+                for (int i = first[c] + 1; i < last[c]; i++) {
+                    seen[s.charAt(i) - 'a'] = true;
+                }
+                for (boolean b : seen) 
+                 if (b)
+                 count++;
+            }
+        }
+        return count;
+    }
+}
