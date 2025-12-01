5b1bb4ce3a7c86ab251c4dc3540c9ca2779ee497
riyatyag <tyagiriya517@gmail.com>
2025-12-01 10:14:45 +0530

    ay 172

--- MaximumRunningTimeofNComputers.java
+++ MaximumRunningTimeofNComputers.java
@@ -55,4 +55,34 @@
  * - The algorithm uses only a few long variables for the binary search and summation.
  */
 // Code - 
-import java.util.*;
+class Solution {
+    public long maxRunTime(int n, int[] batteries) {
+        long sum = 0;
+        for (int b : batteries) 
+        sum += b;
+
+        long left = 0, right = sum / n;
+
+        while (left < right) 
+        {
+            long mid = right - (right - left) / 2;
+
+            if (canRun(mid, batteries, n))
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+    private boolean canRun(long T, int[] batteries, int n) {
+        long total = 0;
+
+        for (int b : batteries)
+         {
+            total += Math.min(b, T);
+            if (total >= (long) n * T) 
+            return true;
+        }
+        return total >= (long) n * T;
+    }
+}
