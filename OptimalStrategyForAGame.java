baa5bd27e61de438b3dd9c90ebaa94f7fd65c187
riyatyag <tyagiriya517@gmail.com>
2025-12-06 10:23:01 +0530

    Day 175

--- OptimalStrategyForAGame.java
+++ OptimalStrategyForAGame.java
@@ -63,3 +63,29 @@
  * - O(N^2) space is required to store the DP table `dp[n][n]`.
  */
 // Code -
+class Solution {
+    public int maximumAmount(int arr[]) {
+        int n = arr.length;
+        int[][] dp = new int[n][n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            dp[i][i] = arr[i];
+        }
+        for (int len = 2; len <= n; len++) 
+        {
+            for (int i = 0; i + len - 1 < n; i++) 
+            {
+                int j = i + len - 1;
+                
+                int x = (i + 2 <= j) ? dp[i + 2][j] : 0;
+                int y = (i + 1 <= j - 1) ? dp[i + 1][j - 1] : 0;
+                int z = (i <= j - 2) ? dp[i][j - 2] : 0;
+                
+                dp[i][j] = Math.max(arr[i] + Math.min(x, y), arr[j] + Math.min(y, z));
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
+
