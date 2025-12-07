7b99cb6609178ae5e41c99f60148f2b578bbc71d
riyatyag <tyagiriya517@gmail.com>
2025-12-07 10:45:11 +0530

    Day 176

--- Count_Odd_Numbers_in_an_Interval_Range.java
+++ Count_Odd_Numbers_in_an_Interval_Range.java
@@ -55,3 +55,14 @@
  * - The algorithm uses only a single integer variable (`count`) for calculation.
  */
 // Code - 
+
+class Solution {
+    public int countOdds(int low, int high)
+     {
+        int count = (high - low) / 2;
+
+        if (low % 2 != 0 || high % 2 != 0)
+        count++;
+        return count;
+    }
+}
