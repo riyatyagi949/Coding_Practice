dc2a60ab7af9462ed1ff6db270a626a210646151
riyatyag <tyagiriya517@gmail.com>
2025-12-15 10:28:01 +0530

    Day 181

--- NumberofSmoothDescentPeriodsofaStock.java
+++ NumberofSmoothDescentPeriodsofaStock.java
@@ -62,3 +62,22 @@
  * `currentLength`, and loop indices), regardless of the input size N.
  */
 // Code - 
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long count = 1;
+        long len = 1;
+
+        for (int i = 1; i < prices.length; i++)
+         {
+            if (prices[i - 1] - prices[i] == 1)
+             {
+                len++;
+            } 
+            else {
+                len = 1;
+            }
+            count += len;
+        }
+        return count;
+    }
+}
