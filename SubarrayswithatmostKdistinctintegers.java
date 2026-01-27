c8e2db3033b9d805356d38678dad4ae8ff3d19e9
RIYA TYAGI <119611030+riyatyagi949@users.noreply.github.com>
2026-01-27 18:18:56 +0530

    Day 216

--- SubarrayswithatmostKdistinctintegers.java
+++ SubarrayswithatmostKdistinctintegers.java
@@ -1,4 +1,4 @@
-// Problem Statement:
+// Problem Statement:-
 // You are given an array arr[] of positive integers and an integer k, find the number of subarrays in arr[] where the count of distinct integers is at most k.
 // Note: A subarray is a contiguous part of an array.
 
